f983cb06c2718879d1f9cd967110012bcf1a2c3d
Leo Okawa Eriscon <leo.ericson@yahoo.se>
2018-11-13 19:46:16 +0100

    Fix helm bindings that broke with exwm

--- doom-private-modules/exwm/config.el
+++ doom-private-modules/exwm/config.el
@@ -2,16 +2,29 @@
 (def-package! exwm
   :config
   (require 'exwm-config)
+;;;; Move between windows
   (setq exwm-input-global-keys `(([?\s-l] . evil-window-right )
                                  ([?\s-h] . evil-window-left )
                                  ([?\s-k] . evil-window-up )
                                  ([?\s-j] . evil-window-down )
                                  ([?\s-b] . helm-buffers-list)))
-
+;;;; Helm bindings
+  (map! :after helm
+        :leader
+        :n "." #'helm-find-files
+        :n "B" #'helm-buffer-list)
+;;;; Multple monitors
+  (require 'exwm-randr)
+  (setq exwm-randr-workspace-output-plist '(0 "VGA-0" 1 "HDMI-0"))
+  (add-hook 'exwm-randr-screen-change-hook
+            (lambda ()
+              (start-process-shell-command
+               "xrandr" nil "xrandr --output VGA1 --left-of LVDS1 --auto")))
+  (exwm-randr-enable)
+;;;; Firefox windows as buffers with better names
   (defun pnh-trim-non-ff ()
     (delete-if-not (apply-partially 'string-match "- Mozilla Firefox$")
                    ido-temp-list))
-
   (add-hook 'exwm-manage-finish-hook
             (defun pnh-exwm-manage-hook ()
               (when (string-match "Firefox" exwm-class-name)
@@ -31,6 +44,7 @@
 
   (setq exwm-input-simulation-keys
       '(([\C-t] . [\C-n])))
+;;;; Initialize
   (exwm-config-default))
 
 
