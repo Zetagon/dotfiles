24945026a348d2c4054d018abd6109282db70fb3
Leo Okawa Ericson <leo.ericson@yahoo.se>
2019-04-14 22:19:02 +0200

    Add reference layer to doom

---
+++ doom-private-modules/reference/config.el
@@ -0,0 +1,63 @@
+;;; config.elReferences and notes -*- lexical-binding: t; -*-
+
+(def-package! org-ref
+  :config
+  (setq org-ref-notes-directory "~/Dropbox/org/references/notes"
+        org-ref-bibliography-notes "~/Dropbox/org/references/articles.org"
+        org-ref-default-bibliography '("~/Dropbox/org/references/Zotero_articles.bib")
+        org-ref-pdf-directory "~/Dropbox/org/references/pdfs/")
+  (map!
+   :leader
+   :n "nn" #'org-ref-open-bibtex-notes))
+
+(def-package! helm-bibtex
+  :config
+  (setq helm-bibtex-bibliography "~/Dropbox/org/references/Zotero_articles.bib"
+        reftex-default-bibliography '("~/Dropbox/org/references/Zotero_articles.bib")
+        helm-bibtex-library-path "~/Dropbox/org/references/pdfs"
+        bibtex-completion-notes-path "~/Dropbox/org/references/notes")
+  (map!
+   :leader
+   :n "nh" #'helm-bibtex
+   :n ))
+
+(def-package! helm-org-rifle
+  :config
+  (setq helm-org-rifle-show-path t)
+  (add-to-list 'display-buffer-alist
+               `(,(rx bos "*helm" (* not-newline) "*" eos)
+                 (display-buffer-in-side-window)
+                 (inhibit-same-window . t)
+                 (window-height . 0.4)))
+
+  (map!
+   :leader
+   :n "nr" (λ! (let ((helm-split-window-inside-p nil))
+                 (helm-org-rifle-directories "~/Dropbox/org/references/notes")))))
+(def-package! pdf-tools
+  :config
+  (map!
+   :map pdf-view-mode-map
+   :n "i" #'interleave-add-note
+   :n "n" #'pdf-view-next-page-command
+   :n "p" #'pdf-view-previous-page-command))
+
+(defun re-seq (regexp string)
+  "Get a list of all regexp matches in a string"
+  (save-match-data
+    (let ((pos 0)
+          matches)
+      (while (string-match regexp string pos)
+        (push (match-string 0 string) matches)
+        (setq pos (match-end 0)))
+      matches)))
+(defun print-radio-toc ()
+  (interactive)
+  (let ((elements (re-seq "<<<.*>>>" (buffer-substring-no-properties 1 (buffer-end 1)))))
+   (apply 'concat
+          (map 'list (lambda (x) (insert (format "%s\n" (substring x 3 -3))))
+               elements))))
+(after! org
+  )
+(provide 'config)
+;;; config.el ends here
---
+++ doom-private-modules/reference/packages.el
@@ -0,0 +1,11 @@
+;;; packages.el --- description -*- lexical-binding: t; -*-
+
+(package! helm-org-rifle)
+(package! interleave)
+(package! org-ref)
+(package! helm-bibtex)
+(package! pdf-tools)
+
+
+(provide 'packages)
+;;; packages.el ends here
