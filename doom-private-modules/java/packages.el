0ff388133c57980db3279a3cd9dfe3e98a45f842
Leo Okawa Ericson <github@relevant-information.com>
2019-09-06 19:16:13 +0200

    Add java lsp layer

---
+++ doom-private-modules/java/packages.el
@@ -0,0 +1,10 @@
+;;; package.el --- description -*- lexical-binding: t; -*-
+
+
+
+(package! lsp-java)
+(package! lsp-mode)
+(package! lsp-ui)
+(package! company-lsp)
+(package! lsp-java-treemacs)
+;;; package.el ends here
