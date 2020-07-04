9c5b2e35b8c311d9b488f3fb8d4b5a89db1b476f
Leo Okawa Eriscon <leo.ericson@yahoo.se>
2020-07-04 22:26:04 +0200

    [pichart] Use correct my/make-pichart function

--- doom/pichart-property.el
+++ doom/pichart-property.el
@@ -57,7 +57,18 @@ CLOCK: [2020-07-04 Sat 20:54]--[2020-07-04 Sat 21:15] =>  0:21
                            :color (or (alist-get k property-colors nil nil #'equal) "black"))
                      property-list))
              property-table)
-    (my/clockreport-pichart (point) property-colors params)))
+    (dolist (entry (reverse property-list))
+      (insert-before-markers
+       "| "
+       (plist-get entry :title)
+       (format " | %s"
+               (org-minutes-to-clocksum-string (plist-get entry :value)))
+       " | "
+       (plist-get entry :color)
+       " | "
+       "\n"))
+    (insert "[[file:/tmp/chart.png]]")
+    (my/make-pichart  property-list "/tmp/chart.png")))
 
 (defun my/clock-sum-only-this-heading ()
   (save-restriction
@@ -66,28 +77,49 @@ CLOCK: [2020-07-04 Sat 20:54]--[2020-07-04 Sat 21:15] =>  0:21
                           (point-max)))
     (org-clock-sum-today)))
 
-(defun my/clockreport-pichart (ipos table-list params property-colors)
-  (interactive)
-  (goto-char ipos)
-  (let ((time-per-tag (make-hash-table
-                       :test 'equal))
-        (tracked-tags (-map (lambda (x) (car x)) my/clockreport-tags-colors))
-        used-tag-stack)
-    (pcase-dolist (`(,file-name ,file-time ,entries) table-list)
-      (pcase-dolist (`(,level ,headline ,tags-list ,ts ,time ,props) entries)
-        (dolist (tag tags-list)
-          (if (-contains? tracked-tags tag)
-              (progn
-                (puthash tag (+ (or time 0)
-                                (or (gethash tag time-per-tag) 0))
-                         time-per-tag)
-                (push tag used-tag-stack))
-            (push nil used-tag-stack)))))
-    (let* ((i 1)
-           tracked-time-plist)
-      (maphash (lambda (k v)
-                 (push (list :title k :value v :color (alist-get k property-colors  nil nil #'equal)) tracked-time-plist))
-               time-per-tag)
-      (my/make-pichart tracked-time-plist "/tmp/chart.png")
-      (insert "[[file:/tmp/chart.png][file:chart.png]]")
-      (org-display-inline-images))))
+(defun my/make-pichart (plots file)
+  (with-current-buffer (find-file-noselect "/tmp/pichart.plt")
+    (delete-region (point-min) (point-max))
+    (insert
+     "\
+set term png
+set output \"" file "\"
+set xrange [-1:1]
+set yrange [-1:1]
+set style fill solid 1
+
+unset border
+unset tics
+set key outside bottom
+")
+
+    (let* ((i 0)
+          (prev-angle 0)
+          angle
+          (value-list (-map (lambda (x) (plist-get x :value)) plots))
+          (sum-values (-sum value-list))
+          (percent-list (-map (lambda (x) (/ (float x) sum-values))
+                              value-list)))
+      (loop for p in plots
+            for percent in percent-list
+            do
+        (setq angle (+ prev-angle (* 360 percent)))
+        (insert (format "$data%d << EOD
+0 0 1 %d %d
+e
+EOD
+"
+                        i
+                        prev-angle
+                        angle))
+        (setq i (+ 1 i))
+        (setq prev-angle angle))
+
+      (setq i 0)
+      (insert "plot ")
+      (dolist (p plots)
+        (insert (format "$data%d" i) " with circles linecolor rgb \"" (or (plist-get p :color) "black") "\" title \"" (plist-get p :title) "\" ,\\\n")
+        (setq i (+ 1 i)))
+      (delete-char -2)
+      (save-buffer)
+      (call-process "gnuplot" nil nil nil "/tmp/pichart.plt"))))
